745f3e9ada9cca3cc5336679591e2ef3b07aa2c0
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-03-06 02:14:10 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,66 +1,29 @@
-# python3
-
 import sys
-import threading
 
-def compute_height(n, parents):
-    tree = [[] for _ in range(n)]
-    root = 0
+def tree_height(n, parent):
+    nodes = [[] for i in range(n)]
     for i in range(n):
-        if parents[i] == -1:
+        if parent[i] == -1:
             root = i
         else:
-            tree[parents[i]].append(i)
-    max_height = 0
-    queue = [root]
-    
-    while queue:
-        height = 0
-        level_size = len(queue)
-        for _ in range(level_size):
-            node = queue.pop(0)
-            children = tree[node]
-            for child in children:
-                queue.append(child)
-        max_height = height if height > max_height else max_height
-        height += 1
-    return max_height
+            nodes[parent[i]].append(i)
+    height = 0
+    stack = [(root, 0)]
+    while stack:
+        node, h = stack.pop()
+        height = max(height, h)
+        for child in nodes[node]:
+            stack.append((child, h+1))
+    return height
 
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-
-def main():
-    # implement input form keyboard and from files
-    input_type = input("Enter F for file or K for keyboard")
-    
-    if input_type == 'F':
-        file_name = input("File name?")
-    # let user input file name to use, don't allow file names with letter a
-        while 'a' in file_name:
-            file_name = input("File name is not valid")
-        try:
-            with open('folder/' + file_name, 'r') as file:
-                n = int(file.readline())
-                parents = list(map(int, file.readline().split()))
-        except:
-            print("Could not read file")
-            return
+if __name__ == "__main__":
+    source = input("Enter F for file or K for keyboard")
+    if source == "F":
+        file = open("input.txt", "r")
+        n = int(file.readline())
+        parent = list(map(int, file.readline().split()))
     else:
-        try:
-            n = int(input("Enter the number of nodes: "))
-            parents = list(map(int, input("Enter the parents of the nodes: ").split()))
-        except:
-            print("Invalid input")
-            return
+        n = int(input("Enter the number of nodes: "))
+        parent = list(map(int, input("Enter the parents of the nodes: ").split()))
 
-    height = compute_height(n, parents)
-
-    # call the function and output it's result
-    print("Height of the tree: ", height)
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+    print(tree_height(n, parent))
